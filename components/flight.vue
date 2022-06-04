0529a0aca1bb1c69b2a025fd10fcc7a43d15605b
Benjamin Rumble <brumble@tucows.com>
2022-06-03 17:11:04 -0700

    added flight details

---
+++ components/flight.vue
@@ -0,0 +1,26 @@
+<template>
+<h2>Flight Details: AC{{flight.flightNumber}}</h2>
+<h3>Status: {{flight.status}}</h3>
+<div>{{flight}}</div>
+<div>
+    <h3>Departure</h3>
+    <strong>Scheduled:</strong> {{flight.departure.scheduledTime}}
+    <strong>Estimated:</strong>{{flight.departure.estimatedTime}}
+</div>
+<div>
+    <h3>Arrival</h3>
+    <strong>Scheduled:</strong> {{flight.arrival.scheduledTime}}
+    <strong>Estimated:</strong>{{flight.arrival.estimatedTime}}
+</div>
+</template>
+<script>
+export default {
+    name: "Flight",
+    props: {
+        flight: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,23 +2,29 @@
 <div>
   <h2>Index</h2>
   <form class="form" @submit.prevent="performSearch">
-      <input type="text" v-model="flightNumber" placeholder="Search" />
+      <input type="text" v-model="flightNumber" placeholder="Search"/>
       <select v-model="date" id="date" name="date">
-        <option v-for="date in dates" :value="date" :key="date">{{ date }}</option>
+        <option v-for="(date, index) in dates" :value="date" :key="date" v-bind:selected="index === 0">{{ date }}</option>
       </select>
 
       <input type="submit" value="Search" />
     </form>
+    <Flight v-if="flight" :flight="flight" />
   </div>
 </template>
 <script setup>
 const useFlightNumber = () => useState("flightNumber");
 const useDates = () => useState("dates");
+const useDate = () => useState("date");
+const useFlight = () => useState("flight");
 
 const flightNumber = useFlightNumber();
 const dates = useDates();
+const date = useDate();
+const flight = useFlight();
 
 dates.value = createDates();
+flightNumber.value = "189";
 
 function pad2(n) {
   return (n < 10 ? '0' : '') + n;
@@ -44,6 +50,7 @@ function createDates(){
     }
     return dates;
 }
+
 async function fetchFlightStatus(flightNumber, date){
     const parser = new DOMParser();
     let url=`https://alpha.irumble.com/aircanadaflightstatus/?forceTimetable=true&flightNumber=${flightNumber}&carrierCode=AC&date=${date}&app_key=AE919FDCC80311DF9BABC975DFD72085&cache=26928`
@@ -58,5 +65,49 @@ async function fetchFlightStatus(flightNumber, date){
 async function performSearch(){
     console.log('performSearch');
     const doc = await fetchFlightStatus(flightNumber.value, dates.value);
+    const flightParsed = docToFlight(doc);
+    console.log('flightParsed', flightParsed);
+    flight.value = flightParsed;
+}
+function docToFlight(doc){
+  let flight = doc.querySelector("Flights");
+  let segment = flight.querySelector("Segments")
+  return {
+    flightNumber: getFlightNumberFromSegment(segment),
+    status: getStatusFromSegment(segment),
+    departure: getDepartureTimesFromSegment(segment),
+    arrival: getArrivalTimesFromSegment(segment)
+  }
+}
+
+function getStatusFromSegment(segment){
+  return segment.querySelector("OverallStatus").childNodes[0].nodeValue;
+}
+ function dateTimeToTime(dateString){
+    return dateString.slice(11,16);
+}
+
+function getDepartureTimesFromSegment(segment){
+ let departureStationInfo = segment.querySelector("DepartureStationInfo");
+ let departureScheduledTime = departureStationInfo.querySelector("ScheduledTime");
+ let departureEstimatedTime = departureStationInfo.querySelector("EstimatedTime");
+ return {
+   scheduledTime: dateTimeToTime(departureScheduledTime.childNodes[0].nodeValue),
+   estimatedTime: dateTimeToTime(departureEstimatedTime.childNodes[0].nodeValue),
+ }
+}
+
+function getArrivalTimesFromSegment(segment){
+  let arrivalStationInfo = segment.querySelector("ArrivalStationInfo");
+  let arrivalScheduledTime = arrivalStationInfo.querySelector("ScheduledTime");
+  let arrivalEstimatedTime = arrivalStationInfo.querySelector("EstimatedTime");
+  return {
+    scheduledTime: dateTimeToTime(arrivalScheduledTime.childNodes[0].nodeValue),
+    estimatedTime: dateTimeToTime(arrivalEstimatedTime.childNodes[0].nodeValue),
+  }
+}
+
+function getFlightNumberFromSegment(segment){
+  return segment.querySelector("FlightNumber").childNodes[0].nodeValue;
 }
 </script>
